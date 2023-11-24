c6692bdb76dde309ff71394d30aa7b735d6d2a7f
miradori <miradori6164@gmail.com>
2023-11-24 17:12:49 +0900

    エラーメッセージの表示設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -120,7 +120,6 @@ RSpec.describe User, type: :model do
     it 'birthdayが空では登録できない' do
       @user.birthday = ''
       @user.valid?
-      binding.pry
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
     end
     #上記異常系のテストコード
