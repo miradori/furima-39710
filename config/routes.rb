f795c359d6085fae165d356f70b21419e0a67d25
miradori <miradori6164@gmail.com>
2023-11-25 17:41:39 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'furimas/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root to: "items#index"
+  resources :items do
+    # resources :comments, only: :create
+  end
 end
