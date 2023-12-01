dc285d266eef12edb978708bcb7024786a9f5f9b
miradori <miradori6164@gmail.com>
2023-12-01 18:40:50 +0900

    モデルの作成とバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,18 @@ class Item < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :burden_id, presence: true
-  validates :area_id, presence: true
-  validates :delivery_id, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :burden_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :delivery
   
 end
