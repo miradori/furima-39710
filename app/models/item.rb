dba441842b62d7fadb4062e60c34150e95f0266f
miradori <miradori6164@gmail.com>
2023-12-11 07:50:48 +0900

    テストコードの作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,13 +11,14 @@ class Item < ApplicationRecord
   belongs_to_active_hash :area
   belongs_to_active_hash :delivery
 
-  validates :title, presence: true
-  validates :description, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :burden_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :delivery_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true, blob: { content_type: :image }
+  validates :title, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :burden_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :area_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, only_integer: true, message: "は¥300から¥9,999,999の範囲内で、半角数値のみが有効です"}
-
+  # validates :user_id, presence: true
 end
